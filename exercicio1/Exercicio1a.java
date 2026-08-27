36dc3736a05efccbcfb2237ec956217c7f95aaf3
Daniel de Oliveira Colonheze <danielcolonhze@gmail.com>
2026-08-27 10:42:02 -0300

    primerio exercicio 1a

---
+++ exercicio1/Exercicio1a.java
@@ -0,0 +1,15 @@
+package exercicio1;
+
+public class Exercicio1a{
+	public static void main(String arg[]){
+		String nome = "Daniel";
+		String curso = "Engenharia de Software";
+		String cidade = "Cornelio Procopio";
+		int idade = 19;
+		
+		System.out.println(nome);
+		System.out.println(curso);
+		System.out.println(cidade);
+		System.out.println(idade);
+	}
+}
