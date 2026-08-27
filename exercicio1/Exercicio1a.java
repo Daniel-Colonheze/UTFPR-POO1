40113737bb25b678c270b81035d193a705d5b166
Daniel de Oliveira Colonheze <danielcolonhze@gmail.com>
2026-08-27 11:07:56 -0300

    exercicio 1 finalizado

---
+++ exercicio1/Exercicio1a.java
@@ -0,0 +1,10 @@
+package exercicio1;
+
+public class Exercicio1a{
+	public static void main(String arg[]){
+		
+		System.out.println("Facim, facim!!!");
+		System.out.println("Fazendo o que o exercicio manda");
+		
+	}
+}
---
+++ exercicio1/Exercicio1c.java
@@ -0,0 +1,10 @@
+package exercicio1;
+
+public class Exercicio1c {
+	public static void main(String arg[]){  // lembrar de passar o nome 
+                                            // como parametro na hora de executar
+		String nome = arg[0];
+		System.out.println("Meu nome e: " + nome);
+		
+	}
+}
